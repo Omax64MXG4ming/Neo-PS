4ad51cffab5cbf70f7dfb6f3b125bf74317e662f
Working <204946399+Omax64MXG4ming@users.noreply.github.com>
2026-08-27 19:40:20 -0600

    Add download links for Neo-PS assets with app.php
    Added links for downloading Neo-PS assets for various platforms.

---
+++ index/app.php
@@ -0,0 +1,13 @@
+<?php
+
+$webIcon = 'icon.png'; // Web Page Icon Obviously The Icon is In This Same Dir
+
+# WARN = THE GETS IS IN USE FOR DASHBOARD AND INDEXLIB'S INCLUDES, THE MODIFY FROM THIS LINKS AFFECT ON DOWNLOADS PAGES AND DASHBOARD DOWNLOAD LINKS.
+$getWindows = 'https://github.com/Omax64MXG4ming/Neo-PS/releases/download/Core/Neo_PS.zip'; 
+
+$getAndroid = 'https://github.com/Omax64MXG4ming/Neo-PS/releases/download/Core/Neo.PS.apk';
+
+$getLauncher = 'https://github.com/Omax64MXG4ming/Neo-PS/releases/download/AndroidLauncher1.1/Neo.Launcher.apk';
+
+$getIos = 'https://github.com/Omax64MXG4ming/Neo-PS/releases/download/iOS-v2/NeoPS.ipa';
+?>
