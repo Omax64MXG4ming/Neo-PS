32a91181866e8cc5192aa93cac2b4e5734475bf1
Working <204946399+Omax64MXG4ming@users.noreply.github.com>
2026-08-28 14:28:59 -0600

    Update dashboard.php

--- database/config/dashboard.php
+++ database/config/dashboard.php
@@ -3,7 +3,7 @@ $homePath = '/../';
 require_once __DIR__."../".$homePath."../index/app.php";
 $installed = true; // Like i said, it changed!
 
-$gdps = "Neo PS"; // Used to title and download
+$gdps = "$gdps"; // Used to title and download
 $lrEnabled = 1; // 1 = Level reupload enabled, 0 = disabled
 $msgEnabled = 1; // 1 = Messenger enabled, 0 = disabled
 $clansEnabled = 1; // 1 = Clans enabled, 0 = disabled
@@ -11,12 +11,12 @@ $songEnabled = 12; // 0 = Song reupload disabled, add 1 to enable song file reup
 $sfxEnabled = 1; // 0 = SFX upload disabled, 1 = enabled
 $convertEnabled = 1; // 1 = Convert SFX to OGG enabled, 0 = disabled
 $songSize = 8; // Max song size in megabytes
-$sfxSize = 6; // Max SFX size in megabytes
+$sfxSize = 5; // Max SFX size in megabytes
 $timeType = 1; // How time will show in-game, 0 - default Cvolton time, 1 - Dashboard-like time, 2 - RobTop-like time
 $dashboardIcon = '../icon.png'; // Icon at the top left of dashboard, can be link
 $dashboardFavicon = '../icon.png'; // Icon in browser's tab, can be link
 $background = '../../bg.png';
-$homeLink = '../dashboard/';
+$homeLink = '../dashboard/';//FIXED DASHBOARD LINK AND ICON BUTTON
 $startLink = '../../';
 $webIcon = 'https://neops.x10.mx/icon.png';
 $listLink = 'https://neops.x10.mx/demonlist/';
@@ -36,65 +36,22 @@ $androidLauncher = "";
 $iosLauncher = "";
 
 $vk = '';
-$gdpshub = 'https://gdpshub.com/gdps/5785';
-$github = 'https://github.com/Omax64MXG4ming/Neo-PS';
-$discord = 'https://discord.gg/TdB33cAyMn';
-$twitter = '';
-$youtube = 'https://youtube.com/@omarstudiosoff?si=BbZOU4yt26jFk8qc';
-$twitch = '';
+$gdpshub = "$gdpshub";
+$github = "$github";
+$discord = "$discord";
+$twitter = "$twitter";
+$youtube = "$youtube";
+$twitch = "$twitch";
 
-$thirdParty[] = array('https://yt3.googleusercontent.com/EZ149IVvU5JX2Fi6yH7R95NQmKdNsea_gggEvJXA0MIZQ397E_WHLLNCgBjL45npnMZNUkpq=s88-c-k-c0x00ffffff-no-rj', 'RobTop', 'https://store.steampowered.com/app/322170/Geometry_Dash/', 'For Geometry Dash');
-$thirdParty[] = array('https://avatars.githubusercontent.com/u/5721187', 'Cvolton', 'https://github.com/Cvolton', 'For GDPS code');
-$thirdParty[] = array('https://avatars.githubusercontent.com/u/52624723', 'Foxodever', 'https://github.com/foxodever/BetterCvoltonGDPS/blob/main/tools/songs/upload.php', 'For file upload script');
+# TY INFO IN : ../../index/app.php CONFIG
+$thirdParty[] = array("$tyIcon1", "$tyUser1", "$tyLink1", "$tyInfo1");
 
-// SFX/Music libraries, syntax is: array(ID (must be unique), LIBRARY NAME, LIBRARY LINK (not to .dat file), LIBRARY TYPE (0 = only SFX, 1 = only music, 2 = both));
-// Template: $customLibrary[] = array(1, '', '', 2); 
+# TY 2 :
 
-$customLibrary[] = array(1, 'Geometry Dash', 'https://geometrydashfiles.b-cdn.net', 2); 
-$customLibrary[] = array(3, $gdps, null, 2); // Your GDPS's library, don't remove it
-$customLibrary[] = array(2, 'Song File Hub', 'https://api.songfilehub.com', 1);
+$thirdParty[] = array("$tyIcon2", "$tyUser2", "$tyLink2", "$tyInfo2");
 
-// SFX converter API links, make one by using code from https://github.com/MegaSa1nt/GDPS-ConvertSFX
-// Template: $convertSFXAPI[] = "";
+# TY 3 :
 
-$convertSFXAPI[] = "https://niko.gcs.skin";
-$convertSFXAPI[] = "https://lamb.gcs.skin";
-$convertSFXAPI[] = "https://omori.gcs.skin"; // You're welcome
-$convertSFXAPI[] = "https://im.gcs.skin";
-$convertSFXAPI[] = "https://hat.gcs.skin";
-$convertSFXAPI[] = "https://converter.m336.dev";
+ $thirdParty[] = array("$tyIcon3", "$tyUser3", "$tyLink3", "$tyInfo3");
 
-$requireAccountForReuploading = false;
-$disallowReuploadingNotUserLevels = false;
-
-/*
-	Cobalt API
-	
-	Use Cobalt API to be able to reupload songs with YouTube links and etc.
-	Requires file upload to be enabled!
-	
-	$useCobalt — Should server use Cobalt to reupload songs by links
-		True — use Cobalt
-		False — don't use Cobalt
-
-	$cobaltAPI[] — links to Cobalt's APIs
-		Server will randomly pick one of Cobalt APIs when reuploading song
-		
-	Turnstile-protected APIs are currently not supported, sorry
-*/
-
-$useCobalt = true;
-$cobaltAPI[] = 'https://cobalt.gcs.skin';
-
-/*
-	Geometry Dash icons renderer Server
-	
-	Dashboard shows icons of players, therefore it requires some server to get icons
-	
-	$iconsRendererServer — what server to use
-	
-	If gdicon.oat.zone doesn't work for you for some reason, you can use icons.gcs.skin
-*/
-
-$iconsRendererServer = 'https://gdicon.oat.zone';
 ?>
