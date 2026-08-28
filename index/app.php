369561fb217c5255b848e2fa3e207cbc5b21d41a
Working <204946399+Omax64MXG4ming@users.noreply.github.com>
2026-08-28 14:24:11 -0600

    Enhance app.php with additional configurations
    Added various configuration links and third-party information.

--- index/app.php
+++ index/app.php
@@ -1,7 +1,7 @@
 <?php
 
-$webIcon = 'icon.png'; // Web Page Icon Obviously The Icon is In This Same Dir
-
+$webIcon = 'icon.png'; // Web Page Icon
+$gdps = 'Neo PS';
 # WARN = THE GETS IS IN USE FOR DASHBOARD AND INDEXLIB'S INCLUDES, THE MODIFY FROM THIS LINKS AFFECT ON DOWNLOADS PAGES AND DASHBOARD DOWNLOAD LINKS.
 $getWindows = 'https://github.com/Omax64MXG4ming/Neo-PS/releases/download/Core/Neo_PS.zip'; 
 
@@ -10,4 +10,61 @@ $getAndroid = 'https://github.com/Omax64MXG4ming/Neo-PS/releases/download/Core/N
 $getLauncher = 'https://github.com/Omax64MXG4ming/Neo-PS/releases/download/AndroidLauncher1.1/Neo.Launcher.apk';
 
 $getIos = 'https://github.com/Omax64MXG4ming/Neo-PS/releases/download/iOS-v2/NeoPS.ipa';
+
+// Dashboard Configs
+
+$gdpshub = 'https://gdpshub.com/gdps/5785';
+$github = 'https://github.com/Omax64MXG4ming/Neo-PS';
+$discord = 'https://discord.gg/TdB33cAyMn';
+$twitter = '';
+$youtube = 'https://youtube.com/@omarstudiosoff?si=BbZOU4yt26jFk8qc';
+$twitch = '';
+
+// Other Links | sponsors
+# How Can Be :
+ 
+# $thirdParty[] = array("$tyIcon1", "$tyUser1", $tyLink1, "$tyInfo1");
+
+$tyIcon1 = 'https://avatars.githubusercontent.com/u/49565695?v=4';
+$tyUser1 = 'MegaSa1nt Core';
+$tyLink1 = 'https://github.com/MegaSa1nt/GMDprivateServer/tree/master';
+$tyInfo1 = 'Neo PS powered by MegaSa1nt Core';
+# TY 2 :
+# $thirdParty[] = array("$tyIcon2", "$tyUser2", $tyLink2, "$tyInfo2");
+$tyIcon2 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZMyTsjd8131zqBJpHd7StPLT7qbZfybaZ3nbKLZI5Lw2pI4wB2ZntICE-&s=10';
+$tyUser2 = 'x10hosting';
+$tyLink2 = 'https://x10hosting.com/';
+$tyInfo2 = 'Neo PS hosted In :';
+# TY 3 :
+# $thirdParty[] = array("$tyIcon3", "$tyUser3", $tyLink3, "$tyInfo3");
+$tyIcon3 = '';
+$tyUser3 = '';
+$tyLink3 = '';
+$tyInfo3 = '';
+
+# LIBRARYS INFO :
+$customLibrary[] = array(1, 'Geometry Dash', 'https://geometrydashfiles.b-cdn.net', 2); 
+$customLibrary[] = array(3, $gdps, null, 2); // Your GDPS's library, don't remove it
+$customLibrary[] = array(2, 'Song File Hub', 'https://api.songfilehub.com', 1);
+
+# I maded My Own Converter :D
+$convertSFXAPI[] = "https://rok.neops.x10.mx";
+$convertSFXAPI[] = "https://niko.gcs.skin";
+$convertSFXAPI[] = "https://lamb.gcs.skin";
+$convertSFXAPI[] = "https://omori.gcs.skin"; // You're welcome
+$convertSFXAPI[] = "https://im.gcs.skin";
+$convertSFXAPI[] = "https://hat.gcs.skin";
+$convertSFXAPI[] = "https://converter.m336.dev";
+
+# Sys Security 
+$requireAccountForReuploading = false;
+$disallowReuploadingNotUserLevels = false;
+
+# Music RP with Cobalt API in Green Cats Server API's Sources
+$useCobalt = true;
+$cobaltAPI[] = 'https://cobalt.gcs.skin';
+
+$iconsRendererServer = 'https://gdicon.oat.zone';
+
+
 ?>
